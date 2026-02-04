76788cce28e1baa9a9f232392520a90ecb913d42
rn-santos227 <rn_santos227@yahoo.com>
2026-02-04 20:19:07 +0800

    feat: created auth store file

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,1 +1,9 @@
 import { defineStore } from 'pinia'
+import type { AuthState, Role } from '../@types/auth'
+
+const STORAGE_KEY = 'ms_auth'
+let hasLoaded = false
+
+export const useAuthStore = defineStore('auth', {
+
+})
