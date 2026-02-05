455340182dd93be07b9d338b91ee51d9ac537b5e
rn-santos227 <rn_santos227@yahoo.com>
2026-02-05 21:42:36 +0800

    feat: created shifts store file

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,5 +1,6 @@
 export const ROUTES = {
   login: '/login',
   adminUsers: '/admin/users',
+  adminSchedule: '/admin/schedule',
   employeeSchedule: '/employee/schedule'
 } as const
---
+++ stores/shifts.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+import type { Shift } from '../@types/shift'
+import shiftsData from '../data/shifts.json'
+
+const STORAGE_KEY = 'ms_shifts'
+const seedShifts = shiftsData as Shift[]
+
+export const useShiftsStore = defineStore('shifts', {
+
+})
